ea588515d946c99c2dbe42ebef1af5725f1c2338
Chao Liu <chaoliu@csail.mit.edu>
2022-05-20 19:15:06 -0400

    Update the format when process pointgrey camera data

--- spinnaker_camera_driver/data/video_data_process.py
+++ spinnaker_camera_driver/data/video_data_process.py
@@ -15,7 +15,7 @@ def main():
 if __name__ == '__main__':
 
     # fetch images from bag file
-    bagfile = "./kitchen-PG4_2022-04-13-17-10-26.bag"
+    bagfile = "./kitchen-PG2_2022-05-20-18-49-27.bag"
     bag = rosbag.Bag(bagfile)
     bridge = CvBridge()
     camera_name = bagfile[bagfile.index('P'):bagfile.index('_')]
@@ -31,10 +31,12 @@ if __name__ == '__main__':
     count = 0
     for topic, msg, t in bag.read_messages(topics="/kitchen/"+camera_name+"/image_raw"):
         cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
-        cv2.imwrite(os.path.join(output_dir, "frame%i.png" % count), cv_img)
+        cv2.imwrite(os.path.join(output_dir, "frame%06i.png" % count), cv_img)
         video.write(np.asarray(cv_img))
+        time_stamp_sec = msg.header.stamp.secs + msg.header.stamp.nsecs/1e9
         stamp_file.write(str(msg.header.stamp.secs) +
                          " " + str(msg.header.stamp.nsecs) + "\n")
+        # stamp_file.write(str(time_stamp_sec) + "\n")
         count += 1
     bag.close()
     cv2.destroyAllWindows()
@@ -44,8 +46,10 @@ if __name__ == '__main__':
     # generate video
     video_name = output_dir + '.avi'
     filenames = os.listdir(output_dir)
-    sorted_filenames = sorted(
-        filenames, key=lambda x: int(x.split('e')[1].split('.')[0]))
+    sorted_filenames = sorted(filenames)
+    # This sorting is used when no zeros in file names
+    # sorted_filenames = sorted(
+    #     filenames, key=lambda x: int(x.split('e')[1].split('.')[0]))
     images = [img for img in sorted_filenames]
     frame = cv2.imread(os.path.join(output_dir, images[0]))
     height, width, layers = frame.shape
